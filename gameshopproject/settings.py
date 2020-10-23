e61bbf03fc53aeaa8dfe5825869eb22904cf7852
minh hoang nguyen <nhminh0701@gmail.com>
2020-10-23 18:43:10 +0700

    Added .env file to store sensitive data

--- gameshopproject/settings.py
+++ gameshopproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e!j(4z*25tpq5zi%-xc8^tytl&hx$yuf2w&_p%#&smc!cf&fli'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
