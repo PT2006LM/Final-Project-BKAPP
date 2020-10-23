1de9da0f9bf118776a1d9716ed713065eec4234b
minh hoang nguyen <nhminh0701@gmail.com>
2020-10-23 18:16:25 +0700

    Initial setup for frontend

---
+++ gameshopproject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gameshopproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameshopproject.settings')
+
+application = get_asgi_application()
