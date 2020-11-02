ad40d4edebbbfe129558c7aac5542ba4e768db1d
nhminh0701 <nhminh0701@gmail.com>
2020-11-02 17:10:32 +0700

    Working on homepage carousel

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,19 +1,32 @@
-const carouselDur = 2000;
+const carouselDur = 5000;
 
 
 var carouselScrollInterval;
 
 startInterval();
+$('.carousel-component[data-component="controller"] input[type="radio"]#0').prop('checked', true);
 
 function startInterval() {
     carouselScrollInterval = setInterval(updateCarousel, carouselDur);
 }
 
-
 $('.carousel').carousel({
     interval: 0
 })
 
+$('.carousel').on('slide.bs.carousel', function(data) {
+    $(`.carousel-component[data-component="controller"] input[type="radio"]#${data.to}`).prop('checked', true);
+})
+
 function updateCarousel() {
     $('.carousel').carousel('next');
+
 }
+
+$('.carousel-component[data-component="controller"] input[type="radio"]').each(function(index) {
+    $(this).on("click", function() {
+        clearInterval(carouselScrollInterval);
+        $('.carousel').carousel(index);
+        startInterval();
+    })
+})
